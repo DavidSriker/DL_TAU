b68d527b812b703b5159c25f1084e130208883e3
DavidSriker <david.sriker@gmail.com>
2020-01-09 11:50:24 +0200

    DataUtilities correction
    trying to fix the code, still not tested

--- LearningHierarchy/LearningPipeline/DataUtilities.py
+++ LearningHierarchy/LearningPipeline/DataUtilities.py
@@ -2,8 +2,10 @@ import os
 import glob
 import re
 import numpy as np
-import cv2  # TODO
-from tensorflow.keras.preprocessing.image import Iterator  # TODO
+import cv2
+import tensorflow as tf
+from tensorflow.data import Dataset
+from tensorflow.keras.preprocessing.image import Iterator
 
 
 def manyExtensionCreate(extenstions):
@@ -28,14 +30,13 @@ class DataExtractor():
     files_type_re_s = manyExtensionCreate(files_type)
 
     def __init__(self, directory):
-
         self.images_path = []
         self.images_gt = []
-
         for sub_dir in sorted(os.listdir(directory)):
             sub_dir_option = os.path.join(directory, sub_dir)
             if os.path.isdir(sub_dir_option):
                 self.getImagesAndLabels(sub_dir_option)
+        self.num_samples = len(self.images_path)
         return
 
     def getImagesAndLabels(self, path):
@@ -55,54 +56,43 @@ class DataExtractor():
             if gt_flag:
                 gt_idx = [int(s) for s in re.findall(r'\d+', ''.join(paths_splitting))]
                 self.images_gt.extend(gt[gt_idx])
-
         return
 
-    class ImagesIterator(Iterator):
-        def __init__(self, directory, new_img_dim=(300, 200, 3), shuffle=True, batch_s=32, seed=2020):
-            data = DataExtractor(directory)
-            num_samples = len(data.images_path)
-            super(ImagesIterator, self).__init__(num_samples, batch_s, shuffle, seed)
+class ImagesIterator(Iterator):
+    def __init__(self, directory, new_img_dim=(300, 200, 3), shuffle=True, batch_s=32, seed=2020):
+        self.data = DataExtractor(directory)
+        self.num_samples = self.data.num_samples
+        self.batch_s = batch_s
+        self.img_dims = (new_img_dim[0], new_img_dim[1])
+        super(ImagesIterator, self).__init__(self.data.num_samples, batch_s, shuffle, seed)
+        return
 
-            return
+    def imagePreperation(self, img_path):
+        img = self.loadImage(img_path)
+        if img.shape != self.new_img_dim:
+            # Linear interpolation for speed and performance
+            img = cv2.resize(img, self.new_img_dim, interpolation=cv2.INTER_LINEAR)
+        return cv2.cvtcolor(img, cv2.COLOR_BGR2RGB)
 
-        def imagePreperation(self, img_path):
-            img = self.loadImage(img_path)
-            if img.shape != self.new_img_dim:
-                # Linear interpolation for speed and performance
-                img = cv2.resize(img, self.new_img_dim, interpolation=cv2.INTER_LINEAR)
-            return cv2.cvtcolor(img, cv2.COLOR_BGR2RGB)
+    def loadImage(self, img_path):
+        return cv2.imread(img_path)
 
-        def loadImage(self, img_path):
-            return cv2.imread(img_path)
-
-    def generateBatches(self, data_dir, validation=False):
+    def generateBatches(self, validation=False):
         seed = random.randint(0, 2 ** 31 - 1)
-        # Load labels, velocities and image filenames. The shuffling will be done after
-        # Load the list of training files into queues
-        iterator = ImagesIterator(directory=data_dir, shuffle=False, batch_s=self.config.batch_size)
-        # Convert to tensors before passing
-        inputs_queue = tf.data.Dataset.from_tensor_slices([iterator.filenames, iterator.ground_truth]). \
-            shuffle(not validation, seed=seed)  # Return the objects of sliced elements
-        dset = tf.data.Dataset.batch(batch_size=self.config.batch_size, drop_remainder=not validation). \
-            map(mapReadImage, num_parallel_calls=self.config.num_threads).prefetch(2)
-        image_batch, pnt_batch = dset
+        inputs_queue = Dataset.from_tensor_slices([self.data.images_path,
+                                                    self.data.images_gt]).shuffle(not validation,
+                                                                                    seed=seed)
+        transformed_data_iter = inputs_queue.map(self.transformData).batch(self.batch_s)
+        return transformed_data_iter
 
-        return [image_batch, pnt_batch], len(iterator.filenames)
-
-    def mapReadImage(self, inputs_queue):
+    def transformData(self, inputs_queue):
         pnt_seq = tf.cast(inputs_queue[1], dtype=tf.float32)
         file_content = tf.io.read_file(inputs_queue[0])
         image_seq = tf.image.decode_jpeg(file_content, channels=3)
-        # Resize images to target size and preprocess them
-        # image_seq = tf.image.resize(image_seq, [self.config.img_height, self.config.img_width])
-        # image_seq = tf.cast(image_seq, dtype=tf.float32)
-        # image_seq = tf.divide(image_seq, 255.0)
         image_seq = self.preprocessImage(image_seq)
-
         return image_seq, pnt_seq
 
-    def preprocessImage(self, image):
+    def preprocessImage(self, img):
         """ Preprocess an input image
         Args:
             Image: A uint8 tensor
@@ -111,8 +101,7 @@ class DataExtractor():
         """
         # tf.image.resize_images
         # ResizeMethod. Defaults to bilinear
-        image = tf.image.resize(image, [self.config.img_height, self.config.img_width])
-        image = tf.cast(image, dtype=tf.float32)
-        image = tf.divide(image, 255.0)
-
-        return image
+        img = tf.image.resize(img, tf.cast(self.img_dims, dtype=tf.float32))
+        img = tf.cast(img, dtype=tf.float32)
+        img = tf.divide(img, 255.0)
+        return img
