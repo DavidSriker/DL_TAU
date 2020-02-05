66526a42a161b56f35d7af03126d84b80a2775bb
root <david.sriker@gmail.com>
2020-02-05 14:07:09 +0000

    added gammas exp part 1

--- Train.py
+++ Train.py
@@ -2,7 +2,7 @@ from LearningHierarchy.LearningPipeline.CommonFlags import *
 from LearningHierarchy.LearningPipeline.BaseLearner import *
 import sys
 
-experiment_num = 1
+experiment_num = 2
 
 
 optimizer_mode = ["Adam",
@@ -25,4 +25,13 @@ elif experiment_num == 1:
         trl = TrajectoryLearner(FLAGS)
         trl.train(opt)
 elif experiment_num == 2:
-    print("TODO")
+    gammas = [0.0001, 0.001, 0.01, 0.1, 1, 10]
+    gammas_str = ["0_0001", "0_001", "0_01", "0_1", "1", "10"]
+    num_epochs = [20, 40, 60, 80, 100]
+    for idx, g in enumerate(gammas):
+        FLAGS.gamma=g
+        FLAGS.checkpoint_dir = os.path.join(FLAGS.checkpoint_dir, "gamma_exp_g_{:}".format(gammas_str[idx]))
+        for e in num_epochs:
+            FLAGS.max_epochs = e
+            trl = TrajectoryLearner(FLAGS)
+            trl.train()
